47c9cae0415e393512bd074540e5b2d1476b6718
Kwang Ho Hur <hurxx018@gmail.com>
2016-08-10 18:04:23 -0700

    Add a folder twitter for the twitter codes and a folder named by 'obsolete'.

---
+++ twitter/get_twitter.py
@@ -0,0 +1,13 @@
+import json, sys, requests
+import simplejson
+from request_oauthlib import OAuth1
+
+def get_100_tweets():
+  return 0
+
+def get_twitter_auth():
+  return 0
+
+def get_twitter_data(auth):
+  return 0
+
